98b313abf7303f0bd364ef12699cbbc3db237646
André <andrevictor50@gmail.com>
2020-11-19 20:15:02 -0300

    adding validations

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-import UsersController from '../controllers/UsersController';
+import { celebrate, Segments, Joi } from 'celebrate';
+import UserController from '../controllers/UsersController';
 
+const userController = new UserController();
 const usersRouter = Router();
-const usersController = new UsersController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create,
+);
 
 export default usersRouter;
