50764f4c7f1d29a905dfc58a7469f0951acc7a03
André <andrevictor50@gmail.com>
2020-11-19 19:40:38 -0300

    adding typeorm

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,14 +1,11 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 
 class UserController {
-  public async create(request: Request, response: Response): Promise<any> {
+  public async create(request: Request, response: Response): Promise<User> {
     const { name, email, password } = request.body;
 
-    const user = {
-      name,
-      email,
-      password,
-    };
+    const user = new User(name, email, password);
 
     return response.json(user);
   }
